e1d0fd6f16436a8d6c42a55f856315a7ab3e10dd
caca endlich <camilaendlichpimenta.trab@gmail.com>
2023-11-11 16:00:55 -0300

    Delete usuario.ts

--- pages/api/usuario.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiHandler, NextApiResponse } from "next";
-import { UserModel } from "@/models/UsersModel";
-import { connectMongoDB } from "@/middlewares/connectMongoDB";
-import { respostaPadrao } from "@/types/respostaPadrao";
-import nc from "next-connect";
